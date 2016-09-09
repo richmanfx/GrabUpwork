ab6a3bea0d20427393a4e1fa38e3b0431cbd344d
Aleksandr Jashhuk <richmanfx@yandex.ru>
2016-09-09 18:07:55 +0300

    Грабер заявок с Upwork.com . Первый коммит.

---
+++ src/main/java/ru/r5am/GrabUpwork.java
@@ -0,0 +1,16 @@
+package ru.r5am;
+
+/**
+ * Created by Aleksandr Jashhuk (Zoer) on 09.09.2016.
+ *
+ */
+
+public class GrabUpwork {
+    public static void main(String[] args) {
+
+        final String nameConfigFile = ".grabupwork/grabupwork.json";            // файл конфигурации
+
+        System.out.println("Configuration file: " + nameConfigFile);
+
+    }
+}
